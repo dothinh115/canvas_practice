556553d4c50c63b573719a390968af207d0c3375
dothinh115 <dothinh115@gmail.com>
2023-10-03 23:23:18 +0700

    up

--- middleware/middleware.global.ts
+++ middleware/middleware.global.ts
@@ -1,5 +1,10 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  const { user } = useAuth();
+  const { user, getProfile } = useAuth();
+  const token = useCookie("access_token");
+  if (token.value && !user.value) {
+    await getProfile();
+    if (!user.value) token.value = null;
+  }
   if (to.name === "dashboard" && !user.value)
     return await navigateTo("/login", { replace: true });
   if (to.name === "login" && user.value)
--- plugins/auth.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtPlugin({
-  name: "get-profile",
-  enforce: "pre",
-  async setup() {
-    const { getProfile } = useAuth();
-    await getProfile();
-  },
-});
